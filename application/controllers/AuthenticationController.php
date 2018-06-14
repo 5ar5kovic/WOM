f4f26af93c2944c37303601283559746f6ca62d3
ivan.jocic <ivan.jocic@mm8.pmf.ni.ac.rs>
2018-06-14 21:01:35 +0200

    Kreiran kontroler Authentication

---
+++ application/controllers/AuthenticationController.php
@@ -0,0 +1,20 @@
+<?php
+
+class AuthenticationController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
